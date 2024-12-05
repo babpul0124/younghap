f5f8cf2930aec638d7266b6ba0646eee1eea6ab5
kwonyunjeong <kwonyunjeong6@daum.net>
2024-12-05 20:15:16 +0900

    Update EventDTO.java

--- src/main/java/persistence/dto/EventDTO.java
+++ src/main/java/persistence/dto/EventDTO.java
@@ -1,6 +1,8 @@
 package persistence.dto;
 
 import java.time.LocalDateTime;
+import java.time.LocalDate;
+
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,5 @@ public class EventDTO
     LocalDateTime startDate;
     LocalDateTime endDate;
     String eventName;
-    LocalDateTime originalCheckOutDate;
-    LocalDateTime checkInDate;
+    LocalDate writed_date;
 }
