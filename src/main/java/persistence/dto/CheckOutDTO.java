3eec74f09f58e7b9b2f596a4484f819c0b9092a7
kwonyunjeong <kwonyunjeong6@daum.net>
2024-12-08 18:53:05 +0900

    Update CheckOutDTO.java

--- src/main/java/persistence/dto/CheckOutDTO.java
+++ src/main/java/persistence/dto/CheckOutDTO.java
@@ -8,10 +8,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CheckOutDTO {
-    private String dormitoryId;
-    private int studentId;
+    private int UserId;
+    private String applicationStatus;
+    private int dormitoryId;
+    private String dormitoryName;
+    private int applicationId;
+    private String isPayment;
     private LocalDateTime checkOutDate;
     private String bankName;
     private String accountNum;
     private String checkOutStatus;
+
+    public CheckOutDTO() {};
 }
