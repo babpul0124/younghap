a95ed4ed29b50004fc537632bc4104c3a83eb8d8
COMkey <rladmsdud4025@naver.com>
2024-12-08 23:55:50 +0900

    로그인 DAO

--- src/main/java/persistence/dao/LoginDAO.java
+++ src/main/java/persistence/dao/LoginDAO.java
@@ -2,55 +2,29 @@ package persistence.dao;
 import java.sql.*;
 import java.util.ArrayList;
 import persistence.dto.LoginDTO;
+import persistence.dto.UserDTO;
 
 public class LoginDAO {
 
     private final Connection connection;
     public LoginDAO(Connection connection) { this.connection = connection;}
 
-    //아이디 패스워드 목록 전송 함수
-    public ArrayList<LoginDTO> getIdPwList() {
-        LoginDTO loginDTO = new LoginDTO();
-
-        ArrayList<LoginDTO> userIdPws = new ArrayList<>();
-
-        String query = "SELECT login_id, password FROM user";
-
-        try (PreparedStatement stmt = connection.prepareStatement(query); ResultSet rs = stmt.executeQuery())
-        {
-            while (rs.next()) {
-
-                loginDTO.setLoginId(rs.getString("login_id"));
-                loginDTO.setPassword(rs.getString("password"));
-
-                userIdPws.add(loginDTO);
-            }
-        }
-        catch (SQLException e) {System.out.println("error: " + e);}
-
-        return userIdPws;
-    }
-
-    public void saveIdPw(String loginId, String password) {
-        LoginDTO loginDTO = new LoginDTO();
-        String query = "INSERT INTO user (login_id, password) VALUES (?, ?)";
-
-        loginDTO.setLoginId(loginId);
-        loginDTO.setPassword(password);
-
+    public UserDTO selectLogInUser(LoginDTO loginDTO) {
+        UserDTO userDto = null;
+        String query = "SELECT * FROM user WHERE user_id = ?"; // SQL 쿼리
         try (PreparedStatement stmt = connection.prepareStatement(query)) {
-            stmt.setString(1, loginDTO.getLoginId()); // login_id 설정
-            stmt.setString(2, loginDTO.getPassword()); // password 설정
+            stmt.setString(1, loginDTO.getLoginId()); // 첫 번째 ?에 userId 설정
 
-            int rowsAffected = stmt.executeUpdate(); // 쿼리 실행
-            if (rowsAffected > 0) {
-                System.out.println("성공적으로 등록함.");
-            } else {
-                System.out.println("등록 못함.");
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    userDto = new UserDTO();
+                    userDto.setUserRole(rs.getString("user_role")); // user_role 저장
+                }
             }
         } catch (SQLException e) {
-            System.out.println("문제 생겼음: " + e.getMessage());
+            System.out.println("Database error: " + e.getMessage());
         }
+        return userDto;
     }
 }
 
