ab8878cdbfc3972147db31216860ac0447c07a06
kwonyunjeong <kwonyunjeong6@daum.net>
2024-12-08 17:15:26 +0900

    CheckInDTO 푸시

---
+++ src/main/java/persistence/dto/CheckInDTO.java
@@ -0,0 +1,32 @@
+package persistence.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+public class CheckInDTO extends DTO{
+    private int dormitoryId;
+    private String dormitoryName;
+    private int userId;
+    private String userName;
+    private int preference;
+    private int mealFrequency;
+    private int mealMoney;
+    private int isSnoring;
+    private String applicationStatus;
+    private LocalDate applicationDate;
+    private int applicationId;
+    private int roomCapacityNum;
+    private int roomNum;
+    private int bedNum;
+    private double grade;
+    private int personalRoomId;
+    private String isPayment;
+    private Byte[] image;
+
+    public CheckInDTO() { };
+}
