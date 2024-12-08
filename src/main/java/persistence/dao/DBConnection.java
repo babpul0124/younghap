e8ba3e2d5cefa86a6e5f2a334ef924522bd92464
kwonyunjeong <kwonyunjeong6@daum.net>
2024-12-08 13:22:06 +0900

    Update DBConnection.java

--- src/main/java/persistence/dao/DBConnection.java
+++ src/main/java/persistence/dao/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class DBConnection {
     private static Connection conn = null;
 
-    private static final String URL = "jdbc:mysql://192.168.56.1/domitory";
+    private static final String URL = "jdbc:mysql:///domitory";
     private static final String USER = "root";  // 사용자명
     private static final String PASSWORD = "20230508";  // 비밀번호
 
